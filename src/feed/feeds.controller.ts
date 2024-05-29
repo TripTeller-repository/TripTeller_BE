e91b833ee00ae87807212d74b5b589ba10957cb7
devellybutton <127278410+devellybutton@users.noreply.github.com>
2024-05-29 15:21:11 +0900

    [🐛fix]: 우리의여행 게시물 기간별조회 API 수정

--- src/feed/feeds.controller.ts
+++ src/feed/feeds.controller.ts
@@ -45,19 +45,39 @@ export class FeedsController {
     }
   }
 
-  // // 모든 공개 게시물 기간별 조회
-  // // feeds/date?startDate=2023-01-01&endDate=2023-01-31&pageNumber=1
-  // @Get('date')
-  // getFeedsByDate(
-  //   @Query('startDate') startDate: string,
-  //   @Query('endDate') endDate: string,
-  //   @Query('pageNumber', ParseIntPipe) pageNumber: number = 1,
-  //   @Query('pageSize', ParseIntPipe) pageSize: number = 9,
-  //   @Req() req: Request,
-  // ) {
-  //   const userId = req.user?.userId;
-  //   return this.feedsService.fetchFeedsByDate(startDate, endDate, pageNumber, pageSize, userId);
-  // }
+  // 모든 공개 게시물 기간별 조회
+  // feeds/date?startDate=2023-01-01&endDate=2023-01-31&pageNumber=1
+  @Get('date')
+  async getFeedsByDate(
+    @Query('startDate') startDate: string,
+    @Query('endDate') endDate: string,
+    @Query('pageNumber', ParseIntPipe) pageNumber: number = 1,
+    @Req() req: Request,
+  ) {
+    try {
+      // 로그인한 상태인 경우 회원의 userId를 추출
+      const authHeader = req.headers['authorization'];
+      let userId = null; // userId 기본값을 null로 설정
+
+      if (authHeader) {
+        const token = authHeader.split(' ')[1];
+        if (token) {
+          const decodedToken = await this.authService.verifyToken(token);
+          userId = decodedToken ? decodedToken.userId : null;
+        }
+      }
+
+      return this.feedsService.fetchFeedsByDate(startDate, endDate, pageNumber, userId || null);
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Failed to fetch public feeds',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
   // 모든 공개 게시글 정렬 : 최신순 (페이지네이션)
   // feeds/order/byRecent?pageNumber=1

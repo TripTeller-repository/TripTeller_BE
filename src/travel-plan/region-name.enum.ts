a0d7ea97323ecaa1756d44887d73b052a50a3958
devellybutton <127278410+devellybutton@users.noreply.github.com>
2024-06-04 18:11:47 +0900

    [✨feat]: search 모듈 추가

---
+++ src/travel-plan/region-name.enum.ts
@@ -0,0 +1,10 @@
+export enum RegionName {
+  Seoul = 'Seoul', // 서울
+  Gangwon = 'Gangwon', // 강원
+  GyeonggiIncheon = 'GyeonggiIncheon', // 경기
+  ChungcheongDaejeon = 'ChungcheongDaejeon', // 충청 대전
+  GyeongsangDaeguUlsan = 'GyeongsangDaeguUlsan', // 경상 대구 울산
+  Busan = 'Busan', // 부산
+  JeollaGwangju = 'JeollaGwangju', // 전라 광주
+  Jeju = 'Jeju', // 제주
+}

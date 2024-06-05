030bd48988ac6aa2457b0a670776431756ce51f4
devellybutton <127278410+devellybutton@users.noreply.github.com>
2024-06-05 15:56:07 +0900

    [🐛fix]: 지역별 게시물 조회는 공개게시물만 가능

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://127.0.0.1:5500'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials: true,
   });
 

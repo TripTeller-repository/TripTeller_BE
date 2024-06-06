1d492b54e1a7ade25ab471edb9ebc77fc5676ebd
devellybutton <127278410+devellybutton@users.noreply.github.com>
2024-06-06 22:53:58 +0900

    [🐛fix]: 미들웨어 로직 수정

--- src/authentication/auth.middleware.ts
+++ src/authentication/auth.middleware.ts
@@ -1,6 +1,7 @@
 import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request, Response, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
@@ -11,11 +12,6 @@ export class AuthMiddleware implements NestMiddleware {
       // 헤더에서 토큰 추출
       const token = req.headers.authorization?.split(' ')[1];
 
-      // 토큰 문자열이 없거나 null인지 확인
-      if (!token || token === '') {
-        throw new UnauthorizedException('토큰이 비어있습니다.');
-      }
-
       // 토큰에서 회원 ID 추출
       const { userId, oauthProvider } = await this.authService.verifyToken(token);
 
@@ -23,12 +19,17 @@ export class AuthMiddleware implements NestMiddleware {
       await this.authService.isWithDrawn(userId);
 
       // request 객체에 회원 id, provider 넣기
-      // 유저 정보를 request 객체에 추가
       req.user = { userId, oauthProvider };
 
       next(); // 다음 미들웨어로 이동
     } catch (error) {
-      next(new UnauthorizedException('토큰 검증에 실패하였습니다.'));
+      const message =
+        error instanceof jwt.TokenExpiredError
+          ? 'Token has expired' // 토큰 유효기간이 만료됨
+          : error instanceof jwt.JsonWebTokenError
+            ? 'Invalid token' // 토큰이 유효하지 않음
+            : 'Token verification error'; // 토큰 검증 중 오류 발생
+      next(new UnauthorizedException(message));
     }
   }
 }
